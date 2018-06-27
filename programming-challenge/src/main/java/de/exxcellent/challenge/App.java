a2c4c57b90547c0f821655a2ad941ef5057c583d
cwang <cwang@i11ssh.iti.kit.edu>
2018-06-27 10:48:45 +0200

    all unimplemented methods has been implemented, and one simple Junit test works correctly

--- programming-challenge/src/main/java/de/exxcellent/challenge/App.java
+++ programming-challenge/src/main/java/de/exxcellent/challenge/App.java
@@ -1,21 +1,22 @@
 package de.exxcellent.challenge;
 
 /**
- * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
- * design. Read: create your own classes and packages as appropriate.
+ * The entry class for your solution. This class is only aimed as starting point
+ * and not intended as baseline for your software design. Read: create your own
+ * classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 public final class App {
+	private static String filePathweath = "src/main/resources/de/exxcellent/challenge/weather.csv";
 
-    public static void main(String... args) {
+	public static void main(String... args) {
 
-        // Your preparation code …
+		// Your preparation code …
+		ChanllengeSolver cs = new ChanllengeSolver();
+		// Your day analysis function call …
+		String dayWithSmallestTempSpread = cs.findSmallTemperatureSpread("Day", "MxT", "MnT", filePathweath);
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
-
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallesGoalSpread);
-    }
+		System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+	}
 }
--- programming-challenge/src/main/java/de/exxcellent/challenge/ChanllengeSolver.java
+++ programming-challenge/src/main/java/de/exxcellent/challenge/ChanllengeSolver.java
@@ -5,43 +5,100 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+
 /**
  * @author Wang
- * @version Version 1.00 time ${27.06.2018} ${10:28} 
+ * @version Version 1.00 time ${27.06.2018} ${10:28}
  */
 public class ChanllengeSolver {
 
 	/**
 	 * find the small temperatureSpread for a given dataset.
-	 * @param dayColum mark the column of the day
-	 * @param MxTColum mark the column of the MxT
-	 * @param MnTColum mark the column of the MnT
-	 * @param filePath the filePath of the dataset
+	 * 
+	 * @param dayColum
+	 *            mark the column of the day
+	 * @param MxTColum
+	 *            mark the column of the MxT
+	 * @param MnTColum
+	 *            mark the column of the MnT
+	 * @param filePath
+	 *            the filePath of the dataset
 	 * @return String of the day number
 	 */
 	public String findSmallTemperatureSpread(String dayColum, String MxTColum, String MnTColum, String filePath) {
 		ArrayList<String[]> dataset = parserData(filePath);
 		return getSolution(dayColum, MxTColum, MnTColum, dataset);
 	}
-	
+
 	/**
 	 * parser the .csv file by a given path
-	 * @param filePath the filePath of the dataset
-	 * @return ArrayList<String[]>, String[] stores rows, then ArrayList<String[]> stores all the data
+	 * 
+	 * @param filePath
+	 *            the filePath of the dataset
+	 * @return ArrayList<String[]>, String[] stores rows, then ArrayList<String[]>
+	 *         stores all the data
 	 */
 	private ArrayList<String[]> parserData(String filePath) {
 		// TODO Auto-generated method stub
 		ArrayList<String[]> content = new ArrayList<>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				content.add(line.split(","));
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return content;
 	}
-	
+
 	/**
 	 * iterate all the data and return the wanted solution
-	 * @param same parameters as function findSmallTemperatureSpread
-	 * @return String of the day number
+	 * 
+	 * @param same
+	 *            parameters as function findSmallTemperatureSpread
+	 * @return String of the day number -1 means error report
 	 */
 	private String getSolution(String dayColum, String MxTColum, String MnTColum, ArrayList<String[]> dataset) {
-		return null;
+		int line = -1; //the number of the row, which has Small Temperature Spread
+		int mxt = -1; //the number of the column, which is mxt
+		int mnt = -1; //the number of the column, which is mnt
+		int day = -1; //the number of the column, which is mnt
+		if (dataset.size() == 0)
+			return String.valueOf(-1);
+		for (int i = 0; i < dataset.get(0).length; i++) {
+			if(dataset.get(0)[i].equals(MxTColum))
+				mxt=i;
+			if(dataset.get(0)[i].equals(MnTColum))
+				mnt=i;
+			if(dataset.get(0)[i].equals(dayColum))
+				day=i;
+		}
+		if((mxt==-1)||(mnt==-1)||(day==-1))
+			return String.valueOf(-1);
+		float min=Float.MAX_VALUE;
+		for(int i=1;i<dataset.size();i++)
+		{
+			try {
+				float mnt_value=Float.parseFloat(dataset.get(i)[mnt]);
+				float mxt_value=Float.parseFloat(dataset.get(i)[mxt]);
+				if(min>Math.abs(mnt_value-mxt_value))
+				{
+					min=Math.abs(mnt_value-mxt_value);
+					line=i;
+				}
+				
+			}catch(NumberFormatException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		//System.out.println(mnt+"   "+mxt+"  "+line);
+		return String.valueOf(dataset.get(line)[day]);
 	}
 
 }
